3f0c390e4f53054bf2764f1d32ea987ace46a906
Lê Viết Thanh Tùng <thanhtung@MacBook-Pro-Cua-Tung.local>
2022-09-29 15:44:48 +0700

    commit

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'order_food'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', '123456789'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "103.138.88.35"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "ezt15163_order_food"),
+      user: env("DATABASE_USERNAME", "ezt15163_root"),
+      password: env("DATABASE_PASSWORD", "VyBgUP[]D%7;"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
