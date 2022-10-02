c9cf10df67fd5ffd2d5cd9bfea900995b5ce7279
Lê Viết Thanh Tùng <thanhtung@192.168.2.16>
2022-10-02 19:26:02 +0700

    commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,22 @@
-module.exports = () => {
-  return {
-    ckeditor: true,
-  };
-};
+// module.exports = () => {
+//   return {
+//     ckeditor: true,
+//   };
+// };
+
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "tungle3001.itfreelancer@gmail.com",
+        defaultReplyTo: "tungle3001.itfreelancer@gmail.com",
+      },
+    },
+  },
+  // ...
+});
