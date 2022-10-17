46c1777ad756f639d3a73f522868374ade03e889
Lê Viết Thanh Tùng <thanhtung@MacBook-Pro-Cua-Tung.local>
2022-10-17 22:06:26 +0700

    commit

--- config/plugins.js
+++ config/plugins.js
@@ -12,6 +12,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         host: env("SMTP_HOST", "smtp.gmail.com"),
         port: env("SMTP_PORT", 587),
+
         auth: {
           user: env("SMTP_USERNAME"),
           pass: env("SMTP_PASSWORD"),
