da4eda26a31a630215acc4bee5192fb1a1b3dc6a
Duongchithang <Chithang0210@gmail.com>
2023-03-06 01:12:11 +0700

    update code back-end

--- config/plugins.js
+++ config/plugins.js
@@ -5,25 +5,39 @@
 // };
 
 module.exports = ({ env }) => ({
-  // ...
-  email: {
-    config: {
-      provider: "nodemailer",
-      providerOptions: {
-        host: env("SMTP_HOST", "smtp.gmail.com"),
-        port: env("SMTP_PORT", 587),
-
-        auth: {
-          user: env("SMTP_USERNAME"),
-          pass: env("SMTP_PASSWORD"),
+  upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
         },
-        // ... any custom nodemailer options
       },
-      settings: {
-        defaultFrom: "hello@eztiec.com",
-        defaultReplyTo: "hello@eztiec.com",
-      },
-    },
-  },
+  // ...
+  // email: {
+  //   config: {
+  //     provider: "nodemailer",
+  //     providerOptions: {
+  //       host: env("SMTP_HOST", "smtp.gmail.com"),
+  //       port: env("SMTP_PORT", 587),
+
+  //       auth: {
+  //         user: env("SMTP_USERNAME"),
+  //         pass: env("SMTP_PASSWORD"),
+  //       },
+  //       // ... any custom nodemailer options
+  //     },
+  //     settings: {
+  //       defaultFrom: "hello@eztiec.com",
+  //       defaultReplyTo: "hello@eztiec.com",
+  //     },
+  //   },
+  // },
   // ...
 });
